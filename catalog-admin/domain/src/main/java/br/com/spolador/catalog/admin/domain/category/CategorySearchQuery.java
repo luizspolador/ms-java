d0737606206410d84cca1f4278574afbe99b0280
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2024-02-28 16:05:12 -0300

    finalizando domain category

---
+++ catalog-admin/domain/src/main/java/br/com/spolador/catalog/admin/domain/category/CategorySearchQuery.java
@@ -0,0 +1,10 @@
+package br.com.spolador.catalog.admin.domain.category;
+
+public record CategorySearchQuery(
+        int page,
+        int perPage,
+        String terms,
+        String sort,
+        String direction
+) {
+}
