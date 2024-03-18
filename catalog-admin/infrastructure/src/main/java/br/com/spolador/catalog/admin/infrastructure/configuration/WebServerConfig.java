86db6196f210e7863e592437a9d33ae4af88a64a
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2024-03-18 09:34:33 -0300

    adicionando spring boot

---
+++ catalog-admin/infrastructure/src/main/java/br/com/spolador/catalog/admin/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package br.com.spolador.catalog.admin.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.spolador.catalog.admin")
+public class WebServerConfig {
+}
